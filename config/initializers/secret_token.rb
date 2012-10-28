77c8514f5903abdbb2e6823427b06415426bf0ba
kyle Simukka <kyle@janrain.com>
2012-10-28 15:27:48 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DmpSite::Application.config.secret_token = '0e6845a9ea51084114022ac6b503ce215102e33e8f03bc6285889ce214871c74d4827304ca46ac85fa11056b5072ddb18f71fc991c42554d5ce79d28d6cb454a'
