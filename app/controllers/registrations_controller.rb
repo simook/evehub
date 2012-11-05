646c4dc751be3f4e40fb3fa1f5a567c318bdbb96
kyle Simukka <kyle@janrain.com>
2012-11-05 09:26:12 -0800

    started work on verifying character api

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -22,4 +22,35 @@ class RegistrationsController < Devise::RegistrationsController
       render "edit"
     end
   end
+
+  def create
+    build_resource
+    apikey = params[:user][:apikey]
+    apisecret = params[:user][:secretkey]
+
+    if verify_eve_api(apikey,apisecret)
+
+    else
+      if resource.save
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_navigational_format?
+          sign_up(resource_name, resource)
+          respond_with resource, :location => after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+          expire_session_data_after_sign_in!
+          respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        respond_with resource
+      end
+    end
+  end
+
+  private
+  def verify_eve_api(key,secret)
+    accessmask = EAAL::API.new(key,secret).APIKeyInfo.key.to_json
+    puts accessmask
+  end
 end
