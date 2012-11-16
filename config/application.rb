8ededbaa92a74d04ecba5bf36d7bfb8ad8f7c4cc
kyle Simukka <kyle@janrain.com>
2012-11-16 09:31:25 -0800

    fresh assets

--- config/application.rb
+++ config/application.rb
@@ -71,6 +71,6 @@ module DmpSite
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.1'
   end
 end
